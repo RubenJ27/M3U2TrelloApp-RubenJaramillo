ca8affceadb676e9350941ef2d408682b9a494c4
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-07-26 16:23:42 -0500

    Se esta terminando la seccion de agregar tareas para su uso

---
+++ js/helpers/createNewtask.js
@@ -0,0 +1,28 @@
+// Apuntamos al formulario para crear la nueva tarea del HTML
+const form = document.querySelector('#formNewTask');
+
+form.addEventListener('submit', (ev) => {
+  // Evitamos que la pagina se recargue cuando se envian los datos
+  ev.preventDefault();
+  const formData = ev.target;
+  console.log(`hola ${formData.deadLineTask.value}`)
+  // Recopilamos la información a enviar a la API
+  const data = {
+    title: formData.titleTask.value,
+    person: formData.responsibleTask.value,
+    details: formData.detailsTask.value,
+    // deadline: formData.deadLineTask.value,
+    deadline: Number(moment().add(formData.deadLineTask.value, 'days').format('X')),
+    created: Number(moment().format("X")),
+    state: 'to-do'
+  };
+  // Hacemos una petición POST para enviar la información a la API y le pasamos el arreglo data con la información nueva
+  axios.post(`${API_URL}/tasks`, data)
+    .then((res) => {
+      //Mandamos la información a la API
+      createTask(res.data);
+      //Reseteamos el formulario
+      formData.reset();
+    })
+    .catch((err) => console.error(err));
+});
